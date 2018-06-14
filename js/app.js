dc10153466be764cf5bb53cb8f474d25166a2358
Shah <pooja.h.shah@capgemini.com>
2018-06-14 19:32:13 +0530

    commit2

--- js/app.js
+++ js/app.js
@@ -3,3 +3,9 @@ function changeProfile()
     $('#chatPage').css({"display":"none"});
     $("#profilePage").css({"display":"inline-block"});
 }
+
+function backToChatPage()
+{
+    $('#chatPage').css({"display":"inline-block"});
+    $("#profilePage").css({"display":"none"});
+}
